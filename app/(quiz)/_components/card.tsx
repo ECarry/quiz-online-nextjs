bd69e10a128e04d117e2e043e475c609dad9161b
ECarry <lianshiliang93@gmail.com>
2024-04-11 22:32:34 +0800

    style: quiz page style

--- app/(quiz)/_components/card.tsx
+++ app/(quiz)/_components/card.tsx
@@ -26,7 +26,7 @@ const Card = ({
     <div
       onClick={onClick}
       className={cn(
-        "h-full border-2 rounded-xl border-b-4 hover:bg-black/5 dark:hover:bg-white/5 p-4 lg:p-6 cursor-pointer active:border-b-2",
+        "h-full border-2 rounded-xl border-b-4 hover:bg-black/5 dark:hover:bg-white/5 p-2 lg:p-4 cursor-pointer flex items-center gap-1",
         selected && "border-sky-300 bg-sky-100 hover:bg-sky-100",
         selected &&
           status === "correct" &&
@@ -38,10 +38,20 @@ const Card = ({
           "pointer-events-none hover:bg-white dark:hover:bg-black active:border-b-4"
       )}
     >
+      <div
+        className={cn(
+          "hidden lg:flex size-5 border-2 items-center justify-center rounded-lg text-neutral-400 lg:text-[15px] text-xs font-semibold",
+          selected && "border-sky-300 text-sky-500",
+          selected && status === "correct" && "border-green-300 text-green-500",
+          selected && status === "wrong" && "border-rose-300 text-rose-500"
+        )}
+      >
+        {shortcut}
+      </div>
       <div className="relative max-h-[80px] lg:max-h-[150px] w-full">
         <p
           className={cn(
-            "text-neutral-600 text-sm lg:text-base",
+            "text-neutral-600 text-sm lg:text-base text-center",
             selected && "text-sky-500",
             selected && status === "correct" && "text-green-500",
             selected && status === "wrong" && "text-rose-500"
@@ -49,18 +59,6 @@ const Card = ({
         >
           {text}
         </p>
-        <div
-          className={cn(
-            "size-5 border-2 flex items-center justify-center rounded-lg text-neutral-400 lg:text-[15px] text-xs font-semibold absolute top-4 -right-2",
-            selected && "border-sky-300 text-sky-500",
-            selected &&
-              status === "correct" &&
-              "border-green-300 text-green-500",
-            selected && status === "wrong" && "border-rose-300 text-rose-500"
-          )}
-        >
-          {shortcut}
-        </div>
       </div>
     </div>
   );
