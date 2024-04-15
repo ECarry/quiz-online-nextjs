0197c1247b60dccb2438e38322634dd15e12b3c1
ECarry <lianshiliang93@gmail.com>
2024-04-15 22:30:26 +0800

    chore: auth page logo

--- components/auth/header.tsx
+++ components/auth/header.tsx
@@ -7,7 +7,10 @@ interface HeaderProps {
 const Header = ({ label }: HeaderProps) => {
   return (
     <div className="w-full flex flex-col gap-y-8 items-center justify-center">
-      <Link href={"/"}>EXAM</Link>
+      <Link href="/">
+        <span className="text-3xl mr-2">💫</span>
+        <span className="font-bold text-lg select-none">Quiz Master Zone</span>
+      </Link>
 
       <p className="text-3xl font-bold">{label}</p>
     </div>
