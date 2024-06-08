8e80608cff1904e97eb64e3741c3f2c7283746ed
ecarry <lianshiliang93@gmail.com>
2024-06-08 16:06:07 +0800

    chore: loading page style

--- components/skeleton.tsx
+++ components/skeleton.tsx
@@ -52,6 +52,10 @@ export const PracticePageSkeleton = () => {
       <div className="flex flex-col gap-4">
         <Skeleton className="h-[120px] w-full" />
         <Skeleton className="h-[120px] w-full" />
+        <Skeleton className="h-[120px] w-full" />
+        <Skeleton className="h-[120px] w-full" />
+        <Skeleton className="h-[120px] w-full" />
+        <Skeleton className="h-[120px] w-full" />
       </div>
     </div>
   );
